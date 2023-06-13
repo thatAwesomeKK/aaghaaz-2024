6b75bb33ea640f8a7252a1979e01e7903589ed56
thatAwesomeKK <32800130+thatAwesomeKK@users.noreply.github.com>
2023-06-14 00:54:03 +0530

    deleted api

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { EventBody } from '@/typings';
+import { data } from '@/utility/data';
 import Image from 'next/image';
 import React from 'react'
 
@@ -8,39 +9,39 @@ type PageProps = {
     };
 };
 
-async function Event({ params: { id } }: PageProps) {
-    const event: EventBody = await fetch(`http://localhost:3000/api/event/${id}`, { next: { revalidate: 60 } }).then(res => res.json())
+function Event({ params: { id } }: PageProps) {
+    const event = data.find((obj) => obj.eventId === id);
     return (
         <div className='flex bg-[#555] overflow-y-hidden h-screen'>
             <div className='flex-1 ml-12 pb-12 scrollbar-hide overflow-y-scroll'>
                 <p className='font-bold text-2xl text-[#f5a7a7a0] mb-20 mt-14 underline'>Know More</p>
                 <div className='relative h-36 mb-10'>
-                    <h3 className='absolute font-extrabold text-5xl sm:text-6xl md:text-7xl text-red-100 z-10'>{event.eventName}</h3>
-                    <h3 className='absolute left-2 font-extrabold text-5xl sm:text-6xl md:text-7xl text-[#b65e5e]'>{event.eventName}</h3>
+                    <h3 className='absolute font-extrabold text-5xl sm:text-6xl md:text-7xl text-red-100 z-10'>{event!.eventName}</h3>
+                    <h3 className='absolute left-2 font-extrabold text-5xl sm:text-6xl md:text-7xl text-[#b65e5e]'>{event!.eventName}</h3>
                 </div>
-                <p className='font-bold text-3xl text-[#de7474]'>Date & Time: <span className='font-medium text-lg text-white'>{event.date} @ {event.time}</span></p>
-                <p className='font-bold text-3xl text-[#de7474]'>Venue: <span className='font-medium text-lg text-white'>{event.venue}</span></p>
+                <p className='font-bold text-3xl text-[#de7474]'>Date & Time: <span className='font-medium text-lg text-white'>{event!.date} @ {event!.time}</span></p>
+                <p className='font-bold text-3xl text-[#de7474]'>Venue: <span className='font-medium text-lg text-white'>{event!.venue}</span></p>
                 <div className='mt-10'>
                     <p className='font-bold text-2xl text-[#de7474]'>Description: </p>
-                    <p className='font-semibold text-lg text-white ml-4 mr-2 mt-2'>{event.description}</p>
+                    <p className='font-semibold text-lg text-white ml-4 mr-2 mt-2'>{event!.description}</p>
                 </div>
                 <div className='mt-10'>
                     <p className='font-bold text-2xl text-[#de7474]'>Rules & Regulations: </p>
-                        {event.rules.map((rule, index) => (
+                        {event!.rules.map((rule, index) => (
                            <p key={index} className='ml-4 my-2 font-semibold text-lg text-white'>-{rule}</p> 
                         ))}
                 </div>
                 <div>
                     <p className='font-bold text-2xl text-[#de7474] mt-10'>Event Coordinators: </p>
                     <div className='flex flex-col space-y-2 mt-2 ml-4'>
-                        {event.contact.s_coord.map((student, k) => (
+                        {event!.contact.s_coord.map((student, k) => (
                             <p key={k} className='text-white font-medium text-lg'>{k + 1}. {student.name} - <span className='text-gray-300'>{student.number}</span></p>
                         ))}
                     </div>
                 </div>
             </div>
             <div className='md:flex-wrap hidden xl:block bg-black h-screen overflow-hidden'>
-                <Image src={event.poster} alt='poster' height={1080} width={710} />
+                <Image src={event!.poster} alt='poster' height={1080} width={710} />
             </div>
         </div>
     )
