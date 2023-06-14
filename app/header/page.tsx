5d80b9af15a8e0e9eaf15d4bb05c2f3dbc44d130
thatAwesomeKK <32800130+thatAwesomeKK@users.noreply.github.com>
2023-06-14 13:55:35 +0530

    Update page.tsx

--- app/header/page.tsx
+++ app/header/page.tsx
@@ -1,18 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
     return (
         <header className='w-screen bg-white flex justify-between'>
-            <div className='relative h-24 w-72'>
+            <Link href="http://imsec.ac.in/" target="_blank" className='relative h-20 md:h-28 w-[90%] md:w-[70%] xl:w-[35%] lg:w-[50%]'>
                 <Image
                     src="https://res.cloudinary.com/personal-cloud-rohaanmd/image/upload/v1650731198/logo_d2gaks.jpg"
                     alt="Logo"
                     fill
                 />
-            </div>
+            </Link>
 
-            <div className='flex flex-col items-center justify-center mr-6 text-xl'>
+            <div className='hidden lg:flex flex-col items-center justify-center mr-6 text-xl'>
                 <p>
                     Visit our {" "}
                     <a className='font-bold' href="http://imsec.ac.in/" target="_blank">
