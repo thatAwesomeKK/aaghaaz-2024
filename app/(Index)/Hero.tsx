8efaee0c86d935328cd3a26b8b4354ba2770f7c3
thatAwesomeKK <32800130+thatAwesomeKK@users.noreply.github.com>
2023-06-16 14:15:09 +0530

    Update Hero.tsx

--- app/(Index)/Hero.tsx
+++ app/(Index)/Hero.tsx
@@ -13,13 +13,12 @@ function Hero() {
             </div>
             <div>
                 <h1 className={`${tilt.className} text-center text-7xl font-bold text-black tracking-wide pt-10`}>AAGHAAZ 2K23</h1>
-                <p className={`${tilt.className} font-bold text-3xl text-black text-center`}>Mega Tech Fusion</p>
                 <div className="flex items-center justify-center h-16 relative">
-                    <hr className="absolute w-[30vw] bg-gray-200" />
-                    <p className="absolute px-3 text-3xl font-bold bg-[#2864b8] text-white">A MANAGEMENT FEST</p>
+                    <p className={`${tilt.className} absolute font-bold text-3xl bg-[#2864b8] z-10 px-2 text-black text-center`}>Mega Tech Fusion</p>
+                    <hr className="absolute w-[90vw] md:w-[50vw] lg:w-[30vw] bg-gray-200" />
                 </div>
                 <p className="-mt-4 text-lg font-bold text-white text-center"> By Department of MBA & MCA</p>
-                    <Link className='flex justify-center animate-pulse font-extrabold text-2xl text-yellow-400 mb-4' href="https://rb.gy/on77s" target='_blank'>Register Here!</Link>
+                <Link className='flex justify-center animate-pulse font-extrabold text-2xl text-yellow-400 mb-4' href="https://rb.gy/on77s" target='_blank'>Register Here!</Link>
             </div>
             <div className='relative h-28 w-28 md:h-52 md:w-52'>
                 <Image className='object-cover' src="/assets/logo.png" alt='logo' fill />
