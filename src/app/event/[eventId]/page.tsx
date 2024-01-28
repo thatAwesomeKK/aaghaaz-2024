3acbb4931e880845688d455b1307b27fa205733e
thatAwesomeKK <2550492.kk@gmail.com>
2024-01-28 20:23:38 +0530

    url update

--- src/app/event/[eventId]/page.tsx
+++ src/app/event/[eventId]/page.tsx
@@ -11,7 +11,7 @@ type PageProps = {
   };
 };
 
-const host = process.env.URL;
+const host = process.env.API_URL;
 
 async function Event({ params: { eventId } }: PageProps) {
   const event: EventBody = await fetch(`${host}/api/event/${eventId}`, {
@@ -126,7 +126,7 @@ async function Event({ params: { eventId } }: PageProps) {
 }
 
 export async function generateStaticParams() {
-  const events: EventBody[] = await fetch(`${process.env.URL}/api/event`).then((res) =>
+  const events: EventBody[] = await fetch(`${host}/api/event`).then((res) =>
     res.json()
   );
 
