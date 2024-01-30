d09060cebe61f77bb688bb4884294fbc43c1c9e0
thatAwesomeKK <32800130+thatAwesomeKK@users.noreply.github.com>
2024-01-30 09:27:43 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
           <p
             className={`${tilt.className} absolute font-bold text-3xl bg-[#2864b8] z-10 px-2 text-black text-center`}
           >
-            Mega Tech Fusion
+            Mega Fest Fusion
           </p>
           <hr className="absolute w-[90vw] md:w-[50vw] lg:w-[30vw] bg-gray-200" />
         </div>
