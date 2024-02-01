ff3834482bd5b5ef41b797bef391fe6538216cc8
thatAwesomeKK <2550492.kk@gmail.com>
2024-02-01 14:54:59 +0530

    commit

--- app/api/event/[id]/route.ts
+++
@@ -1,8 +0,0 @@
-import { data } from "@/utility/data";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request, context: { params: any }) {
-  const { id } = context.params;
-  const found = data.find((obj) => obj.eventId === parseInt(id));
-  return NextResponse.json(found);
-}
